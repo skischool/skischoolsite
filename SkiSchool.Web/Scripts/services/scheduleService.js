c1a44b6124d8b0ef65428babbd0efc89eaa6b507
cnattress <cnattress@gmail.com>
2013-10-17 23:18:34 -0600

    Updated for minification.

---
+++ SkiSchool.Web/Scripts/services/scheduleService.js
@@ -0,0 +1,29 @@
+﻿//module.factory('scheduleService', function ($http, $q) {
+
+//    var _schedules = [];
+//    var _isInit = false;
+//    var _isReady = function () {
+//        return _isInit;
+//    }
+
+//    var _getSchedules = function () {
+//        var deferred = $q.defer();
+
+//        $http.get('../../api/schedules')
+//             .then(function (result) {
+//                 // Success
+//                 angular.copy(result.data, _schedules);
+//                 _isInit = true;
+//                 deferred.resolve();
+//             },
+//             function () {
+//                 // Error
+//                 deferred.reject();
+//             });
+//    }
+
+//    return {
+//        schedules: _schedules,
+//        isReady: _isReady
+//    }
+//});
